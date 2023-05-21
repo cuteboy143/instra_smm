00c2d6e40abbc5a106affad082eaaff222acc696
cuteboy143 <cuteboy143d@gmail.com>
2023-05-21 13:40:43 +0530

    full

---
+++ app/config.php
@@ -0,0 +1,8 @@
+<?php
+//Config Database
+define('DB_HOST', 'localhost');
+define('DB_USER', 'wlijdimy_smartpanel');
+define('DB_PASS', 'Hx1xz9Niwo$*');
+define('DB_NAME', 'wlijdimy_smartpanel');
+define('TIMEZONE', 'Asia/Kolkata');
+define('ENCRYPTION_KEY', 'c5fbb0694cb9c7e6dda5926478149672');
